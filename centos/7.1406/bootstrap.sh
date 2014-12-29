5872dc39591344f2c1f1bf1bfaa447646410e1e0
timwata <webmaster@watatim.com>
2014-12-29 17:38:15 +0900

    add note in bootstrap

--- centos/7.1406/bootstrap.sh
+++ centos/7.1406/bootstrap.sh
@@ -6,7 +6,15 @@ yes | sudo sh /mnt/VBoxLinuxAdditions.run
 sudo umount /mnt
 sudo /etc/init.d/vboxadd setup
 rm /home/vagrant/VBoxGuestAdditions.iso
+
+#
+# cleanup yum cache
+#
 sudo yum clean all
 sudo rm -rf /var/cache/yum
+
+#
+# zero fill for unused disk space
+#
 sudo dd if=/dev/zero of=/EMPTY bs=1M
 sudo rm -rf /EMPTY
