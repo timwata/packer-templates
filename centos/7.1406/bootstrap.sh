74ddd9073ffbd662ca1f0fa1d877e456365899ea
timwata <webmaster@watatim.com>
2014-12-29 14:41:50 +0900

    tweak

--- centos/7.1406/bootstrap.sh
+++ centos/7.1406/bootstrap.sh
@@ -5,4 +5,4 @@ sudo mount -o loop /home/vagrant/VBoxGuestAdditions.iso /mnt
 yes | sudo sh /mnt/VBoxLinuxAdditions.run
 sudo umount /mnt
 sudo /etc/init.d/vboxadd setup
-rm VBoxGuestAdditions.iso
+rm /home/vagrant/VBoxGuestAdditions.iso
